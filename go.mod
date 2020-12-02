8e95907f25dce563124e31be827d06588a02e5d4
Tino Steinort <tinosteinort@live.de>
2020-12-02 22:36:50 +0100

    draw strings and frets

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/tinosteinort/chord-renderer
 
 go 1.14
+
+require (
+	github.com/fogleman/gg v1.3.0
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/llgcode/draw2d v0.0.0-20200930101115-bfaf5d914d1e
+	golang.org/x/image v0.0.0-20200927104501-e162460cd6b5 // indirect
+	gopkg.in/fogleman/gg.v1 v1.3.0 // indirect
+)
--- renderer/renderer.go
+++ renderer/renderer.go
@@ -1,45 +1,66 @@
 package renderer
 
 import (
-	"image"
-	"image/color"
-	"image/png"
-	"os"
-
+	"github.com/fogleman/gg"
 	"github.com/tinosteinort/chord-renderer/chordinfo"
 	"github.com/tinosteinort/chord-renderer/imageinfo"
 )
 
 func Render(chord chordinfo.Chord, imageInfo imageinfo.ImageInfo) error {
 
-	image := &chordImage{image.NewRGBA(image.Rect(0, 0, imageInfo.Width, imageInfo.Height))}
+	dc := gg.NewContext(imageInfo.Width, imageInfo.Height)
 
-	drawImage(image, chord, imageInfo)
+	drawImage(dc, chord, imageInfo)
 
-	imageFile, err := os.Create(imageInfo.TargetFile)
-	if err != nil {
+	if err := dc.SavePNG(imageInfo.TargetFile); err != nil {
 		return err
 	}
-
-	png.Encode(imageFile, image.image)
 
 	return nil
 }
 
-func drawImage(image *chordImage, chord chordinfo.Chord, imageInfo imageinfo.ImageInfo) {
-	bgColor := color.RGBA{100, 200, 200, 255}
+func drawImage(dc *gg.Context, chord chordinfo.Chord, imageInfo imageinfo.ImageInfo) {
 
-	image.Box(0, 0, imageInfo.Width, imageInfo.Height, bgColor)
-}
+	dc.SetRGB255(255, 255, 255)
+	//dc.DrawRectangle(float64(0), float64(0), float64(imageInfo.Width), float64(imageInfo.Height))
+	//dc.Fill()
+	dc.Clear()
 
-type chordImage struct {
-	image *image.RGBA
-}
+	dc.SetRGB255(0, 0, 0)
+	if err := dc.LoadFontFace("/usr/share/fonts/truetype/freefont/FreeSans.ttf", 30); err != nil {
+		panic(err)
+	}
+	dc.DrawStringAnchored(chord.Name, float64(imageInfo.Width/2), 20, 0.5, 0.5)
 
-func (chordImage *chordImage) Box(x int, y int, width int, height int, color color.RGBA) {
-	for y := 0; y < height; y++ {
-		for x := 0; x < width; x++ {
-			chordImage.image.Set(x, y, color)
-		}
+	var sideSpace float64 = 20
+	var bottomSpace float64 = 20
+
+	var nutLineY float64 = 50
+	var nutLineWidth float64 = 10
+	dc.SetRGB255(0, 0, 0)
+	dc.SetLineWidth(nutLineWidth)
+	dc.DrawLine(sideSpace, nutLineY, float64(imageInfo.Width)-sideSpace, nutLineY)
+	dc.Stroke()
+
+	var spaceBetweenStrings float64 = (float64(imageInfo.Width) - (2 * sideSpace)) / float64(chord.StringCount-1)
+	var stringLineWidth float64 = 3
+	for i := 0; i < chord.StringCount; i++ {
+		dc.SetRGB255(0, 0, 0)
+		dc.SetLineWidth(stringLineWidth)
+
+		var x float64 = float64(i)*spaceBetweenStrings + sideSpace
+		dc.DrawLine(x, nutLineY, x, float64(imageInfo.Height)-bottomSpace)
+		dc.Stroke()
+	}
+
+	var spaceBetweenFrets float64 = (float64(imageInfo.Height) - nutLineY - bottomSpace) / float64(chord.FretCount)
+	var fretLineWidth float64 = 3
+	for i := 0; i < chord.FretCount; i++ {
+		dc.SetRGB255(0, 0, 0)
+		dc.SetLineWidth(fretLineWidth)
+
+		var y float64 = float64(i+1) * spaceBetweenFrets
+		dc.DrawLine(sideSpace, nutLineY+y, float64(imageInfo.Width)-sideSpace, nutLineY+y)
+		dc.Stroke()
 	}
 }
